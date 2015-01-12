5899054ba9f397867123535f4cfb7fd7342c6d62
Brad Jewell <brad811@gmail.com>
2015-01-12 01:05:14 -0600

    add keyboard input and player movement

--- web/script.dart
+++ web/script.dart
@@ -1,16 +1,18 @@
 import 'dart:html';
+import 'dart:async';
 
 var canvasWidth = 640,
   canvasHeight = 512;
 
 ImageElement spritesImage;
+CanvasElement c;
 var ctx;
 
 World world;
 Player player;
 
 void main() {
-  CanvasElement c = querySelector('canvas');
+  c = querySelector('canvas');
   ctx = c.getContext("2d");
   ctx.imageSmoothingEnabled = false;
   
@@ -26,6 +28,15 @@ void main() {
 void start() {
   world = new World();
   player = new Player();
+
+  document.onKeyDown.listen((KeyboardEvent e) {
+    if (!Input.keys.contains(e.keyCode))
+      Input.keys.add(e.keyCode);
+  });
+  
+  document.onKeyUp.listen((KeyboardEvent e) {
+    Input.keys.remove(e.keyCode);
+  });
   
   tick();
 }
@@ -34,7 +45,11 @@ void tick() {
   world.render();
   player.render();
   
-  //var timer = new Timer(new Duration(milliseconds: 33), () => print('done'));
+  Input.handleKey();
+  
+  player.tick();
+  
+  new Timer(new Duration(milliseconds: 33), () => tick());
 }
 
 class Tiles {
@@ -65,12 +80,85 @@ class Sprite {
   }
 }
 
+class Input {
+  static var keys = [];
+  
+  static void handleKey() {
+    if(keys.length == 0)
+      return;
+    
+    switch(keys[0]) {
+      case KeyCode.LEFT:
+        player.move(keys[0]);
+        break;
+      case KeyCode.RIGHT:
+        player.move(keys[0]);
+        break;
+      case KeyCode.UP:
+        player.move(keys[0]);
+        break;
+      case KeyCode.DOWN:
+        player.move(keys[0]);
+        break;
+    }
+  }
+}
+
 class Player {
-  var x = 8,
-    y = 5;
+  static final
+    DOWN = 0,
+    RIGHT = 1,
+    UP = 2,
+    LEFT = 3,
+    motionAmount = Sprite.pixelsPerSprite * Sprite.spriteScale;
+  
+  static var 
+    motionX = 0,
+    motionY = 0,
+    motionSpeed = 4,
+    direction = DOWN,
+    x = 8 * motionAmount,
+    y = 5 * motionAmount;
 
   void render() {
-    Sprite.render(Tiles.PLAYER, 1, 2, x, y-1);
+    Sprite.render(Tiles.PLAYER + direction, 1, 2, x/motionAmount, (y-1)/motionAmount);
+  }
+  
+  void move(motionDirection) {
+    if(motionX == 0 && motionY == 0) {
+      if(motionDirection == KeyCode.LEFT) {
+        direction = LEFT;
+        motionX = -motionAmount;
+      } else if(motionDirection == KeyCode.RIGHT) {
+        direction = RIGHT;
+        motionX = motionAmount;
+      } else if(motionDirection == KeyCode.UP) {
+        direction = UP;
+        motionY = -motionAmount;
+      } else if(motionDirection == KeyCode.DOWN) {
+        direction = DOWN;
+        motionY = motionAmount;
+      }
+    }
+  }
+  
+  void tick() {
+    if(motionX < 0) {
+      motionX += motionSpeed;
+      x -= motionSpeed;
+    }
+    else if(motionX > 0) {
+      motionX -= motionSpeed;
+      x += motionSpeed;
+    }
+    else if(motionY < 0) {
+      motionY += motionSpeed;
+      y -= motionSpeed;
+    }
+    else if(motionY > 0) {
+      motionY -= motionSpeed;
+      y += motionSpeed;
+    }
   }
 }
 
