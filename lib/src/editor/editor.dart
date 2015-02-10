2d320af0d86aa9280ddeafc1ac4b54b2debc55ee
Brad Jewell <brad811@gmail.com>
2015-02-10 03:02:12 -0600

    change map tiles to selected tile when clicked

--- lib/src/editor/editor.dart
+++ lib/src/editor/editor.dart
@@ -1,6 +1,5 @@
 library Editor;
 
-import 'dart:async';
 import 'dart:html';
 
 import 'package:dart_rpg/src/character.dart';
@@ -14,6 +13,10 @@ class Editor {
   static ImageElement spritesImage;
   static CanvasElement c, sc, ssc;
   static CanvasRenderingContext2D ctx, sctx, ssctx;
+  
+  static int
+    canvasWidth = 100,
+    canvasHeight = 100;
   
   static List<List<Tile>> renderList;
   static int selectedTile;
@@ -44,6 +47,25 @@ class Editor {
     Main.world.map = [];
     Main.world.characters = [];
     
+    for(int y=0; y<10; y++) {
+      Main.world.map.add([]);
+      for(int x=0; x<10; x++) {
+        Main.world.map[y].add([]);
+        for(int i=0; i<World.layers.length; i++) {
+          Main.world.map[y][x].add(null);
+        }
+      }
+    }
+    
+    for(int y=0; y<10; y++) {
+      for(int x=0; x<10; x++) {
+        Main.world.map[y][x][World.LAYER_GROUND] = new Tile(
+          false,
+          new Sprite.int(Tile.GROUND, x, y)
+        );
+      }
+    }
+    
     sctx.fillStyle = "#ff00ff";
     sctx.fillRect(
       0, 0,
@@ -69,27 +91,45 @@ class Editor {
     
     selectSprite(Tile.GROUND);
     
+    c.onClick.listen((MouseEvent e) {
+      int x = (e.offset.x/Sprite.scaledSpriteSize).floor();
+      int y = (e.offset.y/Sprite.scaledSpriteSize).floor();
+      
+      Main.world.map[y][x][World.LAYER_GROUND] = new Tile(
+        false,
+        new Sprite.int(selectedTile, x, y)
+      );
+      updateMap();
+    });
+    
     sc.onClick.listen((MouseEvent e) {
-      //selectSprite();
       int x = (e.offset.x/Sprite.scaledSpriteSize).floor();
       int y = (e.offset.y/Sprite.scaledSpriteSize).floor();
       selectSprite(y*Sprite.spriteSheetSize + x + 1);
     });
     
-    tick();
+    updateMap();
   }
   
-  static void tick() {
+  static void updateMap() {
+    canvasHeight = Main.world.map.length * Sprite.scaledSpriteSize;
+    canvasWidth = Main.world.map[0].length * Sprite.scaledSpriteSize;
+    
+    c.width = canvasWidth;
+    c.height = canvasHeight;
+    
+    ctx.imageSmoothingEnabled = false;
+    
     // Draw pink background
     ctx.fillStyle = "#ff00ff";
-    ctx.fillRect(0, 0, Main.canvasWidth, Main.canvasHeight);
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     
     renderList = [];
     for(int i=0; i<World.layers.length; i++) {
       renderList.add([]);
     }
     
-    Main.world.render(renderList);
+    renderWorld(renderList);
     
     for(Character character in Main.world.characters) {
       character.render(renderList);
@@ -97,11 +137,12 @@ class Editor {
     
     for(List<Tile> layer in renderList) {
       for(Tile tile in layer) {
-        tile.render();
+        renderStaticSprite(
+          ctx, tile.sprite.id,
+          tile.sprite.posX.round(), tile.sprite.posY.round()
+        );
       }
     }
-
-    new Timer(new Duration(milliseconds: Main.timeDelay), () => tick());
   }
   
   static void selectSprite(int id) {
@@ -125,5 +166,19 @@ class Editor {
       
       Sprite.scaledSpriteSize, Sprite.scaledSpriteSize // width, height
     );
+  }
+  
+  static void renderWorld(List<List<Tile>> renderList) {
+    for(var y=0; y<Main.world.map.length; y++) {
+      for(var x=0; x<Main.world.map[y].length; x++) {
+        for(int layer in World.layers) {
+          if(Main.world.map[y][x][layer] is Tile) {
+            renderList[layer].add(
+              Main.world.map[y][x][layer]
+            );
+          }
+        }
+      }
+    }
   }
 }
