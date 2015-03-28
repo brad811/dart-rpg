f3dc434559d3a5d04c13d7a658d1554f38ba3ff9
Brad Jewell <brad811@gmail.com>
2015-03-28 01:33:11 -0700

    move start menu code to gui class

--- lib/src/player.dart
+++ lib/src/player.dart
@@ -29,57 +29,7 @@ class Player extends Character implements InputHandler {
       interact();
     
     if(keyCodes.contains(Input.START)) {
-      // TODO: move start menu code somewhere else
-      //   and have it only declared once
-      ChoiceGameEvent start;
-      
-      // show start menu
-      GameEvent exit = new GameEvent( (Function a) { Main.focusObject = this; } );
-      
-      GameEvent powers = new GameEvent((Function a) {
-        Gui.windows.add(() {
-          Gui.renderWindow(
-            0, 0,
-            12, 8
-          );
-          
-          Font.renderStaticText(2.0, 2.0, "Player");
-          Font.renderStaticText(2.75, 3.5, "Health: ${battler.startingHealth}");
-          Font.renderStaticText(2.75, 5.0, "Physical Attack: ${battler.startingPhysicalAttack}");
-          Font.renderStaticText(2.75, 6.5, "Physical Defence: ${battler.startingPhysicalDefense}");
-          Font.renderStaticText(2.75, 8.0, "Magical Attack: ${battler.startingMagicalAttack}");
-          Font.renderStaticText(2.75, 9.5, "Magical Defense: ${battler.startingMagicalDefense}");
-          Font.renderStaticText(2.75, 11.0, "Speed: ${battler.startingSpeed}");
-          
-          Font.renderStaticText(2.75, 13.0, "Next Level: ${battler.nextLevelExperience() - battler.experience}");
-        });
-        
-        GameEvent powersBack = new GameEvent((Function a) {
-          Gui.windows = [];
-          start.trigger();
-        });
-        
-        new ChoiceGameEvent.custom(
-            this, {"Back": [powersBack]},
-            15, 0,
-            5, 2
-        ).trigger();
-      });
-      
-      start = new ChoiceGameEvent.custom(
-        this,
-        {
-          "Stats": [exit],
-          "Powers": [powers],
-          "Items": [exit],
-          "Save": [exit],
-          "Exit": [exit]
-        },
-        15, 0,
-        5, 6
-      );
-      
-      start.trigger();
+      Gui.showStartMenu();
     }
     
     for(int key in keyCodes) {
