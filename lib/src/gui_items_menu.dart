87c43a0cd736a8466033fea4cb1a92712ba84f1c
Brad Jewell <brad811@gmail.com>
2015-04-08 00:18:25 -0500

    move item selection screen into a separate class

---
+++ lib/src/gui_items_menu.dart
@@ -0,0 +1,78 @@
+library dart_rpg.gui_items_menu;
+
+import 'package:dart_rpg/src/choice_game_event.dart';
+import 'package:dart_rpg/src/font.dart';
+import 'package:dart_rpg/src/game_event.dart';
+import 'package:dart_rpg/src/gui.dart';
+import 'package:dart_rpg/src/item.dart';
+import 'package:dart_rpg/src/item_stack.dart';
+import 'package:dart_rpg/src/main.dart';
+import 'package:dart_rpg/src/sprite.dart';
+
+class GuiItemsMenu {
+  // TODO: return the item selected
+  // TODO: enable disabling the "back" option
+  
+  static GameEvent exit = new GameEvent((Function callback) {
+    
+  });
+  
+  static GameEvent items = new GameEvent((Function callback) {
+    GameEvent callbackEvent = new GameEvent((Function callback){
+      callback();
+    });
+    exit.callback = callback;
+    
+    Map<String, List<GameEvent>> items = new Map<String, List<GameEvent>>();
+    for(ItemStack itemStack in Main.player.inventory.itemStacks) {
+      String name = itemStack.quantity.toString();
+      if(name.length == 1)
+        name = "0" + name;
+      
+      name += " x ";
+      name += itemStack.item.name;
+      items.addAll({name: [exit]});
+    }
+    items.addAll({"Back": [callbackEvent]});
+    
+    ChoiceGameEvent itemChoice;
+    Function descriptionWindow;
+    
+    GameEvent onCancel = new GameEvent((Function a) {
+      Gui.windows.remove(descriptionWindow);
+      callbackEvent.trigger();
+    });
+    
+    // TODO: somehow use existing logic to break words in other windows
+    GameEvent onChange = new GameEvent((Function callback) {
+      Gui.windows.remove(descriptionWindow);
+      if(itemChoice.curChoice < Main.player.inventory.itemStacks.length) {
+        Item curItem = Main.player.inventory.itemStacks[itemChoice.curChoice].item;
+        Sprite curSprite = new Sprite.int(curItem.pictureId, 13, 1);
+        descriptionWindow = () {
+          Gui.renderWindow(10, 0, 9, 9);
+          Font.renderStaticText(21.0, 9.0, curItem.description);
+          curSprite.renderStaticSized(3, 3);
+        };
+        Gui.windows.add(descriptionWindow);
+      }
+    });
+    
+    itemChoice = new ChoiceGameEvent.custom(
+        Main.player, items,
+        0, 0,
+        10, 10,
+        onCancel,
+        onChange
+    );
+    
+    onChange.trigger();
+    
+    itemChoice.trigger();
+  });
+  
+  static trigger(Function callback) {
+    items.callback = callback;
+    items.trigger();
+  }
+}
--- lib/src/gui_start_menu.dart
+++ lib/src/gui_start_menu.dart
@@ -5,10 +5,8 @@ import 'package:dart_rpg/src/choice_game_event.dart';
 import 'package:dart_rpg/src/font.dart';
 import 'package:dart_rpg/src/game_event.dart';
 import 'package:dart_rpg/src/gui.dart';
-import 'package:dart_rpg/src/item.dart';
-import 'package:dart_rpg/src/item_stack.dart';
+import 'package:dart_rpg/src/gui_items_menu.dart';
 import 'package:dart_rpg/src/main.dart';
-import 'package:dart_rpg/src/sprite.dart';
 
 class GuiStartMenu {
   static ChoiceGameEvent start = new ChoiceGameEvent.custom(
@@ -16,7 +14,7 @@ class GuiStartMenu {
     {
       "Stats": [stats],
       "Powers": [exit],
-      "Items": [items],
+      "Items": [GuiItemsMenu.items],
       "Save": [exit],
       "Exit": [exit]
     },
@@ -60,57 +58,5 @@ class GuiStartMenu {
         5, 2,
         powersBack
     ).trigger();
-  });
-  
-  // TODO: move this into its own class?
-  //   because the item screen will also be used in battle
-  //   perhaps that class could return the item selected
-  static GameEvent items = new GameEvent((Function a) {
-    Map<String, List<GameEvent>> items = new Map<String, List<GameEvent>>();
-    for(ItemStack itemStack in Main.player.inventory.itemStacks) {
-      String name = itemStack.quantity.toString();
-      if(name.length == 1)
-        name = "0" + name;
-      
-      name += " x ";
-      name += itemStack.item.name;
-      items.addAll({name: [exit]});
-    }
-    items.addAll({"Back": [start]});
-    
-    ChoiceGameEvent itemChoice;
-    Function descriptionWindow;
-    
-    GameEvent onCancel = new GameEvent((Function a) {
-      Gui.windows.remove(descriptionWindow);
-      start.trigger();
-    });
-    
-    // TODO: somehow use existing logic to break words in other windows
-    GameEvent onChange = new GameEvent((Function a) {
-      Gui.windows.remove(descriptionWindow);
-      if(itemChoice.curChoice < Main.player.inventory.itemStacks.length) {
-        Item curItem = Main.player.inventory.itemStacks[itemChoice.curChoice].item;
-        Sprite curSprite = new Sprite.int(curItem.pictureId, 13, 1);
-        descriptionWindow = () {
-          Gui.renderWindow(10, 0, 9, 9);
-          Font.renderStaticText(21.0, 9.0, curItem.description);
-          curSprite.renderStaticSized(3, 3);
-        };
-        Gui.windows.add(descriptionWindow);
-      }
-    });
-    
-    itemChoice = new ChoiceGameEvent.custom(
-        Main.player, items,
-        0, 0,
-        10, 10,
-        onCancel,
-        onChange
-    );
-    
-    onChange.trigger();
-    
-    itemChoice.trigger();
   });
 }
