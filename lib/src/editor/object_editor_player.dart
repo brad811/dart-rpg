3f428f25b6ceb49156706a6ed2aef3b5bf919f16
Brad Jewell <brad811@gmail.com>
2015-06-12 21:23:30 -0500

    make player battler type dropdown show battler types

--- lib/src/editor/object_editor_player.dart
+++ lib/src/editor/object_editor_player.dart
@@ -3,7 +3,9 @@ library dart_rpg.object_editor_player;
 import 'dart:async';
 import 'dart:html';
 
+import 'package:dart_rpg/src/battler_type.dart';
 import 'package:dart_rpg/src/main.dart';
+import 'package:dart_rpg/src/world.dart';
 
 class ObjectEditorPlayer {
   static Map<String, StreamSubscription> listeners = {};
@@ -19,11 +21,23 @@ class ObjectEditorPlayer {
       "    <td>Name</td><td>Battler Type</td><td>Level</td><td>X</td><td>Y</td>"+
       "  </tr>"+
       "  <tr>"+
-      "    <td><input type='text' /></td>"+
-      "    <td><select><option>TODO</option></select></td>"+
-      "    <td><input type='text' class='number' /></td>"+
-      "    <td><input type='text' class='number' /></td>"+
-      "    <td><input type='text' class='number' /></td>"+
+      "    <td><input type='text' value='${Main.player.battler.name}' /></td>"+
+      "    <td>";
+      
+    playerHtml += "<select id='object_editor_player_battler_type'>";
+    World.battlerTypes.forEach((String name, BattlerType battlerType) {
+      playerHtml += "<option value='${battlerType.name}'";
+      if(Main.player.battler.name == name) {
+        playerHtml += " selected";
+      }
+      
+      playerHtml += ">${battlerType.name}</option>";
+    });
+    playerHtml += "</select>";
+      
+    playerHtml +=
+      "    </td>"+
+      "    <td><input type='text' class='number' value='${Main.player.battler.level}' /></td>"+
       "  </tr>";
     playerHtml += "</table>";
     querySelector("#player_container").innerHtml = playerHtml;
