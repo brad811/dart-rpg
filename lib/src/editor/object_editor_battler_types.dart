d001a365f636d1ae2ddc26f5d2ad95ee6c23bed1
Brad Jewell <brad811@gmail.com>
2015-07-25 02:51:22 -0500

    move battler type stats and attacks to advanced area

--- lib/src/editor/object_editor_battler_types.dart
+++ lib/src/editor/object_editor_battler_types.dart
@@ -13,9 +13,13 @@ class ObjectEditorBattlerTypes {
   // TODO: give battler types a display name and a unique name
   //   so people can name them stuff like "end_boss_1"
   //   but still have a pretty display name like "Bob"
+  static List<String> advancedTabs = ["battler_type_stats", "battler_type_attacks"];
+  static int selected;
   
   static void setUp() {
+    Editor.setUpTabs(advancedTabs);
     Editor.attachButtonListener("#add_battler_type_button", addNewBattlerType);
+    Editor.attachButtonListener("#add_battler_type_attack_button", addAttack);
   }
   
   static void addNewBattlerType(MouseEvent e) {
@@ -28,82 +32,31 @@ class ObjectEditorBattlerTypes {
     ObjectEditor.update();
   }
   
-  static void update() {
-    String battlerTypesHtml = "<table class='editor_table'>"+
-      "  <tr>"+
-      "    <td>Num</td>"+
-      "    <td>Sprite Id</td>"+
-      "    <td>Name</td>"+
-      "    <td>Base Stats</td>"+
-      "    <td>Level Attacks</td>"+
-      "    <td>Rarity</td>"+
-      "  </tr>";
-    for(int i=0; i<World.battlerTypes.keys.length; i++) {
-      String key = World.battlerTypes.keys.elementAt(i);
-      
-      battlerTypesHtml +=
-        "<tr>"+
-        "  <td>${i}</td>"+
-        "  <td><input class='number' id='battler_type_${i}_sprite_id' type='text' value='${ World.battlerTypes[key].spriteId }' /></td>"+
-        "  <td><input id='battler_type_${i}_name' type='text' value='${ World.battlerTypes[key].name }' /></td>"+
-        
-        "  <td>"+
-        "    <table>"+
-        "      <tr>"+
-        "        <td>Health: </td><td><input class='number' id='battler_type_${i}_health' type='text' value='${ World.battlerTypes[key].baseHealth }' /></td>"+
-        "      </tr><tr>"+
-        "        <td>Speed: </td><td><input class='number' id='battler_type_${i}_speed' type='text' value='${ World.battlerTypes[key].baseSpeed }' /></td>"+
-        "      </tr><tr>"+
-        "        <td>Phys Atk: </td><td><input class='number' id='battler_type_${i}_physical_attack' type='text' value='${ World.battlerTypes[key].basePhysicalAttack }' /></td>"+
-        "      </tr><tr>"+
-        "        <td>Magic Atk: </td><td><input class='number' id='battler_type_${i}_magical_attack' type='text' value='${ World.battlerTypes[key].baseMagicalAttack }' /></td>"+
-        "      </tr><tr>"+
-        "        <td>Phys Def: </td><td><input class='number' id='battler_type_${i}_physical_defense' type='text' value='${ World.battlerTypes[key].basePhysicalDefense }' /></td>"+
-        "      </tr><tr>"+
-        "        <td>Magic Def: </td><td><input class='number' id='battler_type_${i}_magical_defense' type='text' value='${ World.battlerTypes[key].baseMagicalDefense }' /></td>"+
-        "      </tr>"+
-        "    </table>"+
-        "  </td>"+
-        
-        "  <td>";
-      
-      int j = 0;
-      World.battlerTypes[key].levelAttacks.forEach((int level, Attack levelAttack) {
-        battlerTypesHtml += "<input class='number' id='battler_type_${i}_attack_${j}_level' type='text' value='${level}' />";
-        battlerTypesHtml += "<select id='battler_type_${i}_attack_${j}_name'>";
-        World.attacks.forEach((String name, Attack attack) {
-          battlerTypesHtml += "<option ";
-          if(name == levelAttack.name) {
-            battlerTypesHtml += "selected";
-          }
-          battlerTypesHtml += ">${attack.name}</option>";
-        });
-        battlerTypesHtml += "</select>";
-        battlerTypesHtml += "<button id='delete_battler_type_${i}_attack_${j}'>Delete</button><br />";
-        
-        j++;
-      });
-      
-      battlerTypesHtml += "<button id='add_battler_type_${i}_attack'>Add attack</button>";
-        
-      battlerTypesHtml +=
-        "  </td>"+
-        "  <td><input class='number' id='battler_type_${i}_rarity' type='text' value='${ World.battlerTypes[key].rarity }' /></td>"+
-        "  <td><button id='delete_battler_type_${i}'>Delete battler</button></td>" +
-        "</tr>";
+  static void addAttack(MouseEvent e) {
+    BattlerType selectedBattlerType = World.battlerTypes.values.elementAt(selected);
+    for(int i=0; i<100; i++) {
+      if(selectedBattlerType.levelAttacks[i] == null) {
+        selectedBattlerType.levelAttacks[i] = World.attacks.values.first;
+        break;
+      }
     }
-    battlerTypesHtml += "</table>";
-    querySelector("#battler_types_container").setInnerHtml(battlerTypesHtml);
     
-    for(int i=0; i<World.battlerTypes.keys.length; i++) {
-      Editor.attachButtonListener("#add_battler_type_${i}_attack", (MouseEvent e) {
-        BattlerType battlerType = World.battlerTypes.values.elementAt(i);
-        int nextLevel = 1;
-        for(;  battlerType.levelAttacks[nextLevel] != null; nextLevel++);
-        battlerType.levelAttacks[nextLevel] = World.attacks.values.first;
-        Editor.update();
-      });
-    }
+    update();
+    ObjectEditor.update();
+  }
+  
+  static void update() {
+    buildMainHtml();
+    buildStatsHtml();
+    buildAttacksHtml();
+    
+    Editor.attachButtonListener("#add_battler_type_attack_button", (MouseEvent e) {
+      BattlerType battlerType = World.battlerTypes.values.elementAt(selected);
+      int nextLevel = 1;
+      for(;  battlerType.levelAttacks[nextLevel] != null; nextLevel++);
+      battlerType.levelAttacks[nextLevel] = World.attacks.values.first;
+      Editor.update();
+    });
     
     Editor.setMapDeleteButtonListeners(World.battlerTypes, "battler_type");
     
@@ -126,10 +79,154 @@ class ObjectEditorBattlerTypes {
     for(int i=0; i<World.battlerTypes.keys.length; i++) {
       Editor.attachInputListeners("battler_type_${i}", attrs, onInputChange);
       
+      // when a row is clicked, set it as selected and highlight it
+      Editor.attachButtonListener("#battler_type_row_${i}", (Event e) {
+        if(querySelector("#battler_type_row_${i}") != null) {
+          selectRow(i);
+        }
+      });
+      
       for(int j=0; j<World.battlerTypes.values.elementAt(i).levelAttacks.values.length; j++) {
         Editor.attachInputListeners("battler_type_${i}_attack_${j}", attackAttrs, onInputChange);
       }
     }
+  }
+  
+  static void selectRow(int i) {
+    selected = i;
+    
+    for(int j=0; j<World.battlerTypes.keys.length; j++) {
+      // un-highlight other battler type rows
+      querySelector("#battler_type_row_${j}").classes.remove("selected");
+      
+      // TODO: make sure this is right
+      // hide the advanced areas for other battler types
+      querySelector("#battler_type_${j}_stats_table").classes.add("hidden");
+      querySelector("#battler_type_${j}_attacks_table").classes.add("hidden");
+    }
+    
+    // hightlight the selected battler type row
+    querySelector("#battler_type_row_${i}").classes.add("selected");
+    
+    // show the battler types advanced area
+    querySelector("#battler_types_advanced").classes.remove("hidden");
+    
+    // show the advanced tables for the selected battler type
+    querySelector("#battler_type_${i}_stats_table").classes.remove("hidden");
+    querySelector("#battler_type_${i}_attacks_table").classes.remove("hidden");
+  }
+  
+  static void buildMainHtml() {
+    String battlerTypesHtml = "<table class='editor_table'>"+
+      "  <tr>"+
+      "    <td>Num</td>"+
+      "    <td>Sprite Id</td>"+
+      "    <td>Name</td>"+
+      "    <td>Rarity</td>"+
+      "  </tr>";
+    for(int i=0; i<World.battlerTypes.keys.length; i++) {
+      String key = World.battlerTypes.keys.elementAt(i);
+      
+      battlerTypesHtml +=
+        "<tr id='battler_type_row_${i}'>"+
+        "  <td>${i}</td>"+
+        "  <td><input class='number' id='battler_type_${i}_sprite_id' type='text' value='${ World.battlerTypes[key].spriteId }' /></td>"+
+        "  <td><input id='battler_type_${i}_name' type='text' value='${ World.battlerTypes[key].name }' /></td>"+
+        "  <td><input class='number' id='battler_type_${i}_rarity' type='text' value='${ World.battlerTypes[key].rarity }' /></td>"+
+        "  <td><button id='delete_battler_type_${i}'>Delete battler</button></td>" +
+        "</tr>";
+    }
+    battlerTypesHtml += "</table>";
+    querySelector("#battler_types_container").setInnerHtml(battlerTypesHtml);
+  }
+  
+  static void buildStatsHtml() {
+    String html = "";
+    
+    for(int i=0; i<World.battlerTypes.keys.length; i++) {
+      String visibleString = "class='hidden'";
+      if(selected == i) {
+        visibleString = "";
+      }
+      
+      html += "<table id='battler_type_${i}_stats_table' ${visibleString}>";
+      html += "<tr><td>Stat</td><td>Value</td></tr>";
+      BattlerType battlerType = World.battlerTypes.values.elementAt(i);
+      
+      html += "<tr>";
+      html += "<td>Health</td>";
+      html += "<td><input id='battler_type_${i}_health' type='text' class='number' value='${battlerType.baseHealth}' /></td>";
+      html += "</tr>";
+      
+      html += "<tr>";
+      html += "<td>Physical Attack</td>";
+      html += "<td><input id='battler_type_${i}_physical_attack' type='text' class='number' value='${battlerType.basePhysicalAttack}' /></td>";
+      html += "</tr>";
+      
+      html += "<tr>";
+      html += "<td>Physical Defense</td>";
+      html += "<td><input id='battler_type_${i}_physical_defense' type='text' class='number' value='${battlerType.basePhysicalDefense}' /></td>";
+      html += "</tr>";
+      
+      html += "<tr>";
+      html += "<td>Magical Attack</td>";
+      html += "<td><input id='battler_type_${i}_magical_attack' type='text' class='number' value='${battlerType.baseMagicalAttack}' /></td>";
+      html += "</tr>";
+      
+      html += "<tr>";
+      html += "<td>Magical Defense</td>";
+      html += "<td><input id='battler_type_${i}_magical_defense' type='text' class='number' value='${battlerType.baseMagicalDefense}' /></td>";
+      html += "</tr>";
+      
+      html += "<tr>";
+      html += "<td>Speed</td>";
+      html += "<td><input id='battler_type_${i}_speed' type='text' class='number' value='${battlerType.baseSpeed}' /></td>";
+      html += "</tr>";
+      
+      html += "</table>";
+    }
+    
+    querySelector("#battler_type_stats_container").setInnerHtml(html);
+  }
+  
+  static void buildAttacksHtml() {
+    String html = "";
+    
+    for(int i=0; i<World.battlerTypes.keys.length; i++) {
+      String visibleString = "class='hidden'";
+      if(selected == i) {
+        visibleString = "";
+      }
+      
+      html += "<table id='battler_type_${i}_attacks_table' ${visibleString}>";
+      html += "<tr><td>Level</td><td>Attack</td><td></td></tr>";
+      BattlerType battlerType = World.battlerTypes.values.elementAt(i);
+      
+      int j=0;
+      battlerType.levelAttacks.forEach((int level, Attack attack) {
+        html += "<tr><td>";
+        html += "<input class='number' id='battler_type_${i}_attack_${j}_level' type='text' value='${level}' />";
+        html += "</td><td>";
+        html += "<select id='battler_type_${i}_attack_${j}_name'>";
+        World.attacks.keys.forEach((String name) {
+          html += "<option ";
+          if(name == attack.name) {
+            html += "selected";
+          }
+          html += ">${name}</option>";
+        });
+        html += "</select>";
+        html += "</td><td>";
+        html += "<button id='delete_battler_type_${i}_attack_${j}'>Delete</button><br />";
+        html += "</td></tr>";
+        
+        j += 1;
+      });
+      
+      html += "</table>";
+    }
+    
+    querySelector("#battler_type_attacks_container").setInnerHtml(html);
   }
   
   static void onInputChange(Event e) {
