027549dc32f62db891e00deb51876d0cdd60848d
Brad Jewell <brad811@gmail.com>
2015-08-04 15:37:19 -0500

    give the settings tab a little style

--- lib/src/editor/settings.dart
+++ lib/src/editor/settings.dart
@@ -3,9 +3,9 @@ library dart_rpg.settings;
 import 'dart:html';
 
 class Settings {
-  static List<String> settingsTabs = [];
-  static Map<String, DivElement> settingsTabDivs = {};
-  static Map<String, DivElement> settingsTabHeaderDivs = {};
+  static List<String> tabs = [];
+  static Map<String, DivElement> tabDivs = {};
+  static Map<String, DivElement> tabHeaderDivs = {};
   
   static void init() {
   }
@@ -14,6 +14,15 @@ class Settings {
   }
   
   static void update() {
+    buildMainHtml();
+  }
+  
+  static void buildMainHtml() {
+    String html = "";
+    
+    html += "Settings will go here.";
+    
+    querySelector("#settings_main_tab").setInnerHtml(html);
   }
   
   static void export(Map<String, Map<String, Map<String, Object>>> exportJson) {
