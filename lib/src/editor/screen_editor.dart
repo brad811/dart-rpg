8d2bc397fb5a321a1e6979fa36d0891388d41b92
Brad Jewell <brad811@gmail.com>
2016-01-30 14:50:26 -0600

    set up screen editor a little

--- lib/src/editor/screen_editor.dart
+++ lib/src/editor/screen_editor.dart
@@ -9,7 +9,38 @@ import 'package:react/react.dart';
 class ScreenEditor extends Component {
   static CanvasElement screenCanvas;
   static CanvasRenderingContext2D ctx;
+
+  componentDidMount(Element rootNode) {
+    screenCanvas = querySelector("#screen_editor_canvas");
+    ctx = screenCanvas.getContext("2d");
+
+    setUpCanvas();
+
+    // TODO: do this with CSS instead
+    Function resizeFunction = (Event e) {
+      querySelector('#screen_editor_left').style.width = "${window.innerWidth - 662}px";
+      querySelector('#screen_editor_left').style.height = "${window.innerHeight - 60}px";
+    };
+    
+    window.onResize.listen(resizeFunction);
+    resizeFunction(null);
+  }
+
+  componentDidUpdate(Map prevProps, Map prevState, Element rootNode) {
+    setUpCanvas();
+  }
+
+  setUpCanvas() {
+    Main.fixImageSmoothing(screenCanvas, Main.canvasWidth, Main.canvasHeight);
+
+    ctx.fillStyle = "#ff00ff";
+    ctx.fillRect(0, 0, Main.canvasWidth, Main.canvasHeight);
+  }
   
+  void update() {
+    setState({});
+  }
+
   render() {
     return tr({'id': 'screen_editor_tab'}, [
       td({'id': 'screen_editor_left'},
@@ -19,35 +50,6 @@ class ScreenEditor extends Component {
         div({'className': 'tab'})
       )
     ]);
-  }
-
-  static void init() {
-    screenCanvas = querySelector("#screen_editor_canvas");
-    ctx = screenCanvas.getContext("2d");
-  }
-  
-  static void setUp() {
-    Function resizeFunction = (Event e) {
-      querySelector('#screen_editor_left').style.width = "${window.innerWidth - 662}px";
-      querySelector('#screen_editor_left').style.height = "${window.innerHeight - 60}px";
-    };
-    
-    window.onResize.listen(resizeFunction);
-    resizeFunction(null);
-  }
-  
-  static void update() {
-    // TODO
-    buildMainHtml();
-    
-    Main.fixImageSmoothing(screenCanvas, Main.canvasWidth, Main.canvasHeight);
-    
-    ctx.fillStyle = "#ff00ff";
-    ctx.fillRect(0, 0, Main.canvasWidth, Main.canvasHeight);
-  }
-  
-  static void buildMainHtml() {
-    // TODO
   }
   
   static void export(Map<String, Map<String, Map<String, Object>>> exportJson) {
