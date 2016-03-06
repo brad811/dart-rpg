5c21ade7bff519a317a2c86214a7921bd115febb
Brad Jewell <brad811@gmail.com>
2016-03-05 21:41:56 -0600

    make warp tiles use character warp function and enter function of the destination tile

--- lib/src/warp_tile.dart
+++ lib/src/warp_tile.dart
@@ -5,6 +5,7 @@ import 'package:dart_rpg/src/gui.dart';
 import 'package:dart_rpg/src/main.dart';
 import 'package:dart_rpg/src/sprite.dart';
 import 'package:dart_rpg/src/tile.dart';
+import 'package:dart_rpg/src/world.dart';
 
 class WarpTile extends Tile {
   String destMap;
@@ -17,17 +18,18 @@ class WarpTile extends Tile {
     Gui.fadeDarkAction(() {
       Main.world.curMap = destMap;
       
-      character.map = destMap;
-      
-      character.x = destX * Sprite.pixelsPerSprite * Sprite.spriteScale;
-      character.y = destY * Sprite.pixelsPerSprite * Sprite.spriteScale;
-      character.mapX = destX;
-      character.mapY = destY;
+      character.warp(destMap, destX, destY, character.layer, character.direction);
       
       character.motionX = 0;
       character.motionY = 0;
     }, () {
       Main.player.inputEnabled = true;
+
+      try {
+        Main.world.maps[destMap].tiles[destY][destX][World.LAYER_GROUND].enter(character);
+      } on RangeError catch(_) {
+        // do nothing
+      }
     });
   }
 }
