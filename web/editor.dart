f5fd1316f5156b73e39ceac849e1ba66a5ebd1a3
Brad Jewell <brad811@gmail.com>
2016-03-14 16:59:18 -0500

    update react

--- web/editor.dart
+++ web/editor.dart
@@ -2,13 +2,14 @@ import 'dart:html';
 
 import 'package:dart_rpg/src/editor/editor.dart';
 
-import 'package:react/react.dart';
 import 'package:react/react_client.dart' as reactClient;
+import 'package:react/react.dart';
+import 'package:react/react_dom.dart' as reactDom;
 
 var editor = registerComponent(() => new Editor());
 
 void main() {
   reactClient.setClientConfiguration();
 
-  render(editor({}), querySelector('#editor_container'));
+  reactDom.render(editor({}), querySelector('#editor_container'));
 }
