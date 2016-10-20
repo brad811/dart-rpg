648300ba77b5f838f8861e26e03ee16538a81efb
Brad Jewell <brad811@gmail.com>
2016-10-19 23:02:35 -0500

    add gamepad configuration and support

--- lib/src/game_screen/title_screen.dart
+++ lib/src/game_screen/title_screen.dart
@@ -2,6 +2,10 @@ library dart_rpg.title_screen;
 
 import 'dart:html';
 
+import 'package:dart_rpg/src/font.dart';
+import 'package:dart_rpg/src/gui.dart';
+import 'package:dart_rpg/src/input.dart';
+import 'package:dart_rpg/src/input_handler.dart';
 import 'package:dart_rpg/src/main.dart';
 import 'package:dart_rpg/src/sprite.dart';
 import 'package:dart_rpg/src/tile.dart';
@@ -11,7 +15,7 @@ import 'package:dart_rpg/src/game_event/choice_game_event.dart';
 
 import 'package:dart_rpg/src/game_screen/game_screen.dart';
 
-class TitleScreen extends GameScreen {
+class TitleScreen extends GameScreen implements InputHandler {
   TitleScreen() {
     // TODO: move to editor
     for(int y=0; y<Main.world.viewYSize; y++) {
@@ -20,6 +24,59 @@ class TitleScreen extends GameScreen {
         backgroundTiles[y].add(new Tile(false, new Sprite.int(66, x, y)));
       }
     }
+  }
+
+  static int mode = -1;
+
+  @override
+  void handleKeys(List<int> keyCodes) {
+    if(keyCodes.length != 1 && mode != 0)
+      return;
+
+    if(Input.lastGamepad == null || Input.lastGamepad.length == 0 || mode == -1)
+      return;
+
+    String buttonText = "";
+    if(mode == 0) {
+      buttonText = "CONFIRM";
+    } else if(mode == 1) {
+      Input.CONFIRM_BUTTON = keyCodes[0];
+      buttonText = "BACK";
+    } else if(mode == 2) {
+      Input.BACK_BUTTON = keyCodes[0];
+      buttonText = "LEFT";
+    } else if(mode == 3) {
+      Input.LEFT_BUTTON = keyCodes[0];
+      buttonText = "RIGHT";
+    } else if(mode == 4) {
+      Input.RIGHT_BUTTON = keyCodes[0];
+      buttonText = "UP";
+    } else if(mode == 5) {
+      Input.UP_BUTTON = keyCodes[0];
+      buttonText = "DOWN";
+    } else if(mode == 6) {
+      Input.DOWN_BUTTON = keyCodes[0];
+      buttonText = "START";
+    } else if(mode == 7) {
+      Input.START_BUTTON = keyCodes[0];
+      mode = -1;
+      Gui.clear();
+      this.trigger();
+      return;
+    }
+
+    mode++;
+
+    Gui.clear();
+    Gui.addWindow(() {
+      Gui.renderWindow(
+        0, 5,
+        20, 5
+      );
+      
+      Font.renderStaticText(2.0, 14.0, "Configuring: ${ Input.lastGamepad.substring(0, 31) }");
+      Font.renderStaticText(2.0, 17.0, "Press a button for ${ buttonText }");
+    });
   }
   
   @override
@@ -32,7 +89,8 @@ class TitleScreen extends GameScreen {
   void trigger() {
     // TODO: enable other custom choice game event borders and text alignments
     Map<String, List<GameEvent>> choices = {
-      "New Game": [new GameEvent(newGame)]
+      "New Game": [new GameEvent(newGame)],
+      "Configure Input": [new GameEvent(configureInput)]
     };
     
     if(window.localStorage.containsKey("saved_game")) {
@@ -44,12 +102,28 @@ class TitleScreen extends GameScreen {
     ).trigger(this);
   }
   
-  void newGame(callback) {
+  void newGame(Function callback) {
     Main.onTitleScreen = false;
     Main.focusObject = Main.player;
   }
+
+  void configureInput(Function callback) {
+    Gui.clear();
+    Gui.addWindow(() {
+      Gui.renderWindow(
+        0, 5,
+        20, 5
+      );
+      
+      Font.renderStaticText(2.0, 14.0, "Press a button on the controller you would like");
+      Font.renderStaticText(2.0, 15.5, "to configure.");
+    });
+
+    Main.focusObject = this;
+    mode = 0;
+  }
   
-  void loadGame(callback) {
+  void loadGame(Function callback) {
     Main.world.loadGameProgress();
     Main.world.curMap = Main.player.getCurCharacter().map;
     Main.onTitleScreen = false;
--- lib/src/input.dart
+++ lib/src/input.dart
@@ -4,6 +4,7 @@ import 'dart:html';
 
 import 'package:dart_rpg/src/input_handler.dart';
 import 'package:dart_rpg/src/main.dart';
+import 'package:dart_rpg/src/game_screen/title_screen.dart';
 
 class Input {
   static final int
@@ -14,7 +15,7 @@ class Input {
     UP = KeyCode.UP,
     DOWN = KeyCode.DOWN,
     START = KeyCode.ENTER;
-  
+
   static List<int>
     keys = [],
     lastKeys = [],
@@ -27,9 +28,82 @@ class Input {
       DOWN,
       START
     ],
+    allKeys = [
+      CONFIRM, BACK,
+      LEFT, RIGHT, UP, DOWN,
+      START
+    ],
     validKeys = [];
+
+
+  static String lastGamepad = "";
+
+  static int
+    CONFIRM_BUTTON = -1,
+    BACK_BUTTON = -1,
+    LEFT_BUTTON = -1,
+    RIGHT_BUTTON = -1,
+    UP_BUTTON = -1,
+    DOWN_BUTTON = -1,
+    START_BUTTON = -1;
+  
+  static List<int>
+    buttons = [],
+    lastButtons = [],
+    releasedButtons = [
+      CONFIRM_BUTTON, BACK_BUTTON,
+      LEFT_BUTTON, RIGHT_BUTTON, UP_BUTTON, DOWN_BUTTON,
+      START_BUTTON
+    ],
+    validButtons = [];
   
   static void handleKey(InputHandler focusObject) {
+
+    // Start Buttons
+    buttons = [];
+
+    List<Gamepad> gamepads = window.navigator.getGamepads();
+    gamepads.forEach((Gamepad gamepad) {
+      if(gamepad == null)
+        return;
+
+      List<GamepadButton> gamepadButtons = gamepad.buttons;
+      for(int i=0; i<gamepadButtons.length; i++) {
+        if(gamepadButtons[i].pressed) {
+          lastGamepad = gamepad.id;
+          buttons.add(i);
+        } else if(!releasedButtons.contains(i)) {
+          releasedButtons.add(i);
+        }
+      }
+    });
+
+    for(int i=0; i<lastButtons.length; i++) {
+      if(!buttons.contains(lastButtons[i]) && !releasedButtons.contains(lastButtons[i])) {
+        releasedButtons.add(lastButtons[i]);
+      }
+    }
+
+    validButtons = [];
+    for(int i=0; i<buttons.length; i++) {
+      if(
+        releasedButtons.contains(buttons[i]) ||
+        // only allow holding buttons down if you're controlling the player
+        (Main.focusObject == Main.player &&
+          (buttons[i] == LEFT_BUTTON || buttons[i] == RIGHT_BUTTON ||
+          buttons[i] == UP_BUTTON || buttons[i] == DOWN_BUTTON ||
+          buttons[i] == BACK_BUTTON
+          )
+        )
+      ) {
+        validButtons.add(buttons[i]);
+        releasedButtons.remove(buttons[i]);
+      }
+    }
+
+    lastButtons = new List<int>.from(buttons);
+    // End Buttons
+
     // find keys that have been released
     for(int i=0; i<lastKeys.length; i++) {
       if(!keys.contains(lastKeys[i]) && !releasedKeys.contains(lastKeys[i])) {
@@ -55,7 +129,30 @@ class Input {
       }
     }
     
-    focusObject.handleKeys(validKeys);
+    if(TitleScreen.mode == -1) {
+      for(int i=0; i<validButtons.length; i++) {
+        if(!validKeys.contains(validButtons[i])) {
+          if(validButtons[i] == CONFIRM_BUTTON)
+            validKeys.add(CONFIRM);
+          else if(validButtons[i] == BACK_BUTTON)
+            validKeys.add(BACK);
+          else if(validButtons[i] == LEFT_BUTTON)
+            validKeys.add(LEFT);
+          else if(validButtons[i] == RIGHT_BUTTON)
+            validKeys.add(RIGHT);
+          else if(validButtons[i] == UP_BUTTON)
+            validKeys.add(UP);
+          else if(validButtons[i] == DOWN_BUTTON)
+            validKeys.add(DOWN);
+          else if(validButtons[i] == START_BUTTON)
+            validKeys.add(START);
+        }
+      }
+
+      focusObject.handleKeys(validKeys);
+    } else {
+      focusObject.handleKeys(validButtons);
+    }
     
     lastKeys = new List<int>.from(keys);
   }
